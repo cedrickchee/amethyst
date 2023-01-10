e02751d3a643d6974f350b0fe2aa4328d79ae870
Cedric Chee <cedric@invictusbyte.com>
2023-01-10 23:17:39 +0800

    add code comments in menu-reset.js

--- assets/quartz/js/menu-reset.js
+++ assets/quartz/js/menu-reset.js
@@ -1,3 +1,10 @@
+/**
+ * This JS is not part of Quartz originally.
+ * 
+ * We added this JS to workaround an issue where
+ * we need to restore menu position as soon as possible to avoid flickering.
+ */
+
 (function() {
   var menu = document.querySelector("aside .book-menu-content");
   addEventListener("beforeunload", function(event) {
